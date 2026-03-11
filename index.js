39aed3b7674c09c27b4395f549d0de2470e0d149
felippecastro55-blip <felippecastro55@gmail.com>
2026-03-11 16:12:39 -0300

    Button remover

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ var buttonEditar = `<a href="#" class="editar">
                                 d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
                         </svg>
                         </a>`;
+var buttonRemover = `<a href="#" class="delete">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+                        </svg>
+                     </a>`
+                    
 var statusTarefa = `<div class="form-check form-switch">
                             <input class="form-check-input status" type="checkbox">
                         </div>`;
@@ -25,13 +31,14 @@ var statusTarefa = `<div class="form-check form-switch">
 for (let i = 0; i < localStorage.length; i++) {
     let tarefaStorage = JSON.parse(localStorage.getItem(localStorage.key(i)));
     $('tbody').append(`<tr>
-                                <td name="id" hidden></td>
-                                <td name="data"></td>
-                                <td name="hora"></td>
-                                <td name="tarefa"></td>
+                            <td name="id" hidden></td>
+                            <td name="data"></td>
+                            <td name="hora"></td>
+                            <td name="tarefa"></td>
                             <td></td>
-                            <td>${buttonEditar}</td>
-                            </tr>`);
+                            <td style="text-align: center;">${buttonEditar}</td>
+                            <td>${buttonRemover}</td>
+                        </tr>`);
     Object.keys(tarefaStorage).forEach(function (el, j) {
 
         if (el !== "status") {
@@ -66,7 +73,8 @@ $("#salvar").on('click', function () {
                             <td name="hora">${hora.val()}</td>
                             <td name="tarefa">${tarefa.val()}</td>
                             <td>${statusTarefa}</td>
-                            <td>${buttonEditar}</td>
+                            <td style="text-align: center;">${buttonEditar}</td>
+                            <td>${buttonRemover}</td>
                             </tr>`);
 
     objTarefas.id = id
@@ -122,4 +130,25 @@ $('tbody').on('click', '.status', function () {
     }
     localStorage.setItem(`tarefa${id}`, JSON.stringify(tarefaLocalStrg));
 });
-//var req = new XMLHttpRequest();
+//Button remover
+$('tbody').on('click', '.delete', function(){
+    console.log("clicou em deletar");
+    let idTarefa = $(this).closest('tr').find('[name="id"]').text();
+    localStorage.removeItem(`tarefa${idTarefa}`);
+    $(this).closest('tr').remove();
+});
+
+
+/* var req = new XMLHttpRequest();
+req.open('GET', 'teste.txt', true)
+req.onload = function(){
+    console.log(req.responseText);
+};
+req.send(); */
+
+var req = new XMLHttpRequest();
+req.open('GET', 'teste.txt', true)
+req.onload = function () {
+    console.log(req.responseText);
+};
+req.send();
